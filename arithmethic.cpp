ded109581a218d599de6afb56163b15d498fad43
Muhammad Adhitya <135214464+lexxadev@users.noreply.github.com>
2023-09-06 22:11:42 +0700

    Update arithmethic.cpp

--- arithmethic.cpp
+++ arithmethic.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 int main () {
-    
+
     float x, y, result;
     char mathSymbol;
 
@@ -13,7 +13,7 @@ int main () {
     cout << "Masukan angka : "; // Masukan angka yang diinginkan 
     cin >> x;
 
-    cout << "Pilih operasi perhitungan : "; // Pilih salah satu operasi perhitungan seperti "+, -, *, /, %"
+    cout << "Pilih operasi perhitungan (+, -, *, /, %) : "; // Pilih salah satu operasi perhitungan seperti "+, -, *, /, %"
     cin >> mathSymbol;
 
     cout << "Masukan angka sekali lagi : ";
@@ -42,16 +42,17 @@ int main () {
         break;
 
         case '%':
-        result = result = static_cast<int>(x) % static_cast<int>(y); // Gunakan " % " untuk menggunakan operasi modulus
+        result = static_cast<int>(x) % static_cast<int>(y); // Gunakan " % " untuk menggunakna operasi modulus
         cout << x << " % " <<  y  << " = " << result << endl; 
         break;
 
         default :
         cout << "Operasi perhitungan tidak valid!" << endl;
-    
+
     }
 
     cout << "===============================" << endl;
 
+    cin.get();
     return 0;
 }
